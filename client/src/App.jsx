8c02f1acda104090dca4861cd7e40a8490b44b71
THARANIPRAKASH <tharaniprakashp2006@gmail.com>
2025-12-18 07:45:23 +0530

    feat: develop categories page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Footer from './components/Footer'
 import { useAppContext } from './context/AppContext'
 import Login from './components/Login.jsx'
 import AllProducts from './pages/AllProducts.jsx'
+import ProductCategory from './pages/ProductCategory.jsx'
 
 const App = () => {
 
@@ -25,6 +26,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/products' element={<AllProducts />} />
+          <Route path='/products/:category' element={<ProductCategory />} />
         </Routes>
       </div>
 
