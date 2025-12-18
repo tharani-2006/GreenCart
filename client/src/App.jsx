e8c4eef581a9aa6cf5b231ac5193313e940be236
THARANIPRAKASH <tharaniprakashp2006@gmail.com>
2025-12-18 10:21:41 +0530

    develop product details component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import { useAppContext } from './context/AppContext'
 import Login from './components/Login.jsx'
 import AllProducts from './pages/AllProducts.jsx'
 import ProductCategory from './pages/ProductCategory.jsx'
+import ProductDetails from './pages/ProductDetails.jsx'
 
 const App = () => {
 
@@ -27,6 +28,7 @@ const App = () => {
           <Route path='/' element={<Home />} />
           <Route path='/products' element={<AllProducts />} />
           <Route path='/products/:category' element={<ProductCategory />} />
+          <Route path='/products/:category/:id' element={<ProductDetails />} />
         </Routes>
       </div>
 
