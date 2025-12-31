31ccd2df048f21cfe24dd6bffaf3f82a92153af8
THARANIPRAKASH <tharaniprakashp2006@gmail.com>
2025-12-31 14:48:17 +0530

    create route for login user and test

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -43,3 +43,45 @@ export const register = async (req, res) => {
         res.status(500).json({ success: false, message: "Server Error" });
     }
 };
+
+//Login User : /api/user/login
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if(!email || !password) {
+            return res.json({succes:false, message: "All fields are required"});
+        }
+
+        const user = await User.findOne({ email });
+
+        if(!user) {
+            return res.json({success: false, message: "Invalid email or password"});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch) {
+            return res.json({success: false, message: "Invalid email or password"});
+        }
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: "7d"});
+
+        res.cookie("token", token, {
+            httpOnly: true, // prevent JavaScript to access cookie
+            secure : process.env.NODE_ENV === "production", // use secure cookies in production
+            sameSite : process.env.NODE_ENV === "production" ? "none" : "strict", //CSRF protection
+            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days for cookie expiry
+        })
+
+        return res.json({success: true, message: "User logged in successfully", user: {
+            id: user._id,
+            name: user.name,
+            email: user.email,
+        }})
+
+    } catch (error) {
+        console.error("Error in user login:", error);
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
+};
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { register } from '../controllers/userController.js'
-
+import { login } from '../controllers/userController.js'
 
 const userRouter = express.Router()
 
 userRouter.post('/register', register)
+userRouter.post('/login', login)
 
 export default userRouter
