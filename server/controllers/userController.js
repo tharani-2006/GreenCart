45b565bcf153ec5ccc42e7835fda6f0e522e677f
THARANIPRAKASH <tharaniprakashp2006@gmail.com>
2025-12-31 22:22:56 +0530

    add auth route and logout functionality

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -98,3 +98,18 @@ export const isAuth = async (req, res) => {
     }
 };
 
+//Logout User : /api/user/logout
+export const logout = async (req, res) => {
+    try {
+        res.clearCookie("token", {
+            httpOnly: true,
+            secure : process.env.NODE_ENV === "production",
+            sameSite : process.env.NODE_ENV === "production" ? "none" : "strict",
+        });
+        return res.json({success: true, message: "User logged out successfully"});
+    }
+    catch (error) {
+        console.error("Error in user logout:", error);
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
+}
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { register } from '../controllers/userController.js'
+import { register, login } from '../controllers/userController.js'
 import { login } from '../controllers/userController.js'
+import { isAuth } from '../middlewares/authMiddleware.js'
 
 const userRouter = express.Router()
 
 userRouter.post('/register', register)
 userRouter.post('/login', login)
+userRouter.get('/is-auth', isAuth)
 
 export default userRouter
