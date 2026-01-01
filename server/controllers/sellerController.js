bb2aa4dfadbd722ee3643f06359aac803c16e142
THARANIPRAKASH <tharaniprakashp2006@gmail.com>
2026-01-01 13:54:44 +0530

    create sellerLogin controller

---
+++ server/controllers/sellerController.js
@@ -0,0 +1,25 @@
+
+
+// Login Seller : /api/seller/login
+
+export const sellerLogin = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+
+        if(password === process.env.SELLER_PASSWORD && email === process.env.SELLER_EMAIL) {
+            const token = jwt.sign({email: email}, process.env.JWT_SECRET, {expiresIn: "7d"});
+            res.cookie("sellerToken", token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production",
+                sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+                maxAge: 7 * 24 * 60 * 60 * 1000,
+            });
+            return res.json({success: true, message: "Seller logged in successfully"});
+        } else {
+            return res.json({success: false, message: "Invalid email or password"});
+        }
+    } catch (error) {
+        console.error("Error in seller login:", error);
+        res.status(500).json({success: false, message: "Server Error"});
+    }
+}
