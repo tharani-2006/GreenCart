ef66cbf0319b7ddb851d99f37737a89434d6d6fa
THARANIPRAKASH <tharaniprakashp2006@gmail.com>
2026-01-06 20:24:36 +0530

    create routes for address with authUser verification

---
+++ server/routes/addressRoute.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { addAddress, getAddresses, updateAddress, deleteAddress } from '../controllers/addressController.js';
+import authUser from '../middlewares/authUser.js';
+
+const addressRouter = express.Router();
+
+// All address routes are protected
+addressRouter.post('/add', authUser, addAddress);
+addressRouter.get('/all', authUser, getAddresses);
+addressRouter.post('/update/:id', authUser, updateAddress);
+addressRouter.delete('/delete/:id', authUser, deleteAddress);
+
+export default addressRouter;
+
